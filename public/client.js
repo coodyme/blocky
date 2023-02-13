6d6465bbc53cc96ca102c3c30f5a8805f29cb05d
augustobritodev <augustobritodev@icloud.com>
2023-02-12 21:55:18 -0300

    chore: fix socket

--- public/client.js
+++ public/client.js
@@ -1,63 +1,130 @@
 import manager from './manager.js'
+import { 
+  PREFIX_CLIENT,
+  EVENT_CONNECT,
+  EVENT_UPDATE_PLAYERS,
+  EVENT_SEND_MESSAGE,
+  EVENT_SEND_MESSAGE_GLOBAL,
+  EVENT_PREVIOUS_MESSAGES,
+  EVENT_RECEIVE_MESSAGE
+ } from './constants.js'
+import { toggleElement } from './utils.js'
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementsByClassName('canvas')[0];
 const context = canvas.getContext('2d');
-const chatMessage = document.getElementById('chat-message');
-const chatContent = document.getElementById('chat-content');
+const userInput = document.getElementById('user-input');
+const playButton = document.getElementById('play-button');
+const roomButton = document.getElementById('room-button');
 
-const playersUl = document.getElementById('players');
+const chatMessages = document.getElementById('chat-messages');
+const chatMessageInput = document.getElementById('chat-message-input');
+const chatMessageButton = document.getElementById('chat-message-button');
 
-const PREFIX = '[LORBY CLIENT]';
+const statusPlayersList = document.getElementById('status-players-list');
 
 const socket = io()
-
 const gameManager = manager()
 
 const skins = ['lorbi', 'milos', 'lorbiroto', 'lorbilove']
 
+userInput.addEventListener('click', event => {
+  if (userInput.value) {
+    userInput.value = ''
+  }
+})
+
+playButton.addEventListener('click', () => {
+  window.alert('keep calm, lorby is coming soon')
+  return
+  toggleElement('menu-page')
+  toggleElement('game-page')
+})
+
+roomButton.addEventListener('click', () => {
+  const user = userInput.value
+  if (!user) {
+    userInput.value = 'Please enter a name'
+    return
+  }
+  toggleElement('menu-page')
+  toggleElement('chat-page')
+})
+
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+context.font = "48px serif";
+context.fillStyle = "red";
+context.fillText("Welcome to Lorbiroto Place", 150, 150);
+
 function drawMessage(message) {
   let p = document.createElement('p')
   p.innerHTML = `${message.author}: ${message.content}`
-  chatContent.appendChild(p)
+  chatMessages.appendChild(p)
+  chatMessages.scrollTop = chatMessages.scrollHeight
+  
 }
 
-socket.on('connect', () => {
+function drawGlobalMessage(message) {
+  let p = document.createElement('p')
+  p.innerHTML = `${message.author}: ${message.content}`
+  chatMessages.appendChild(p)
+  chatMessages.scrollTop = chatMessages.scrollHeight
+}
+
+socket.on(EVENT_CONNECT, () => {
   const playerId = socket.id
-  console.log(`${PREFIX} Player connected on Client with id: ${playerId}`)
-  //playersUl.innerHTML = gameManager.players.map(player => {player.name}).join('')
+  console.log(`${PREFIX_CLIENT} Player connected on Client with id: ${playerId}`)
   gameManager.players.push(playerId)
   document.title = playerId
+})
 
-  // Draw Player
+socket.on(EVENT_UPDATE_PLAYERS, players => {
+  statusPlayersList.innerHTML = ''
+  for (let player of players) {
+    let li = document.createElement('li')
+    li.innerHTML = player.id
+    statusPlayersList.appendChild(li)
+  }
+
+  console.log(players)
+
   for(let player in gameManager.players) {
     let img = document.createElement('img')
     let skin = skins[Math.floor(Math.random() * skins.length)]
     img.src = `./assets/${skin}.png`
+    let x = Math.floor(Math.random() * canvas.width)
+    let y = Math.floor(Math.random() * canvas.height)
     img.onload = () => {
-      context.drawImage(img, Math.random(10,50), Math.random(10,50), 50, 50)
+      context.drawImage(img, x, y, 50, 50)
     }
   }
 })
 
-socket.on('previousMessages', messages => {
+socket.on(EVENT_PREVIOUS_MESSAGES, messages => {
   for (let message of messages) {
     drawMessage(message)
   }
 })
 
 
-socket.on('receivedMessage', message => {
+socket.on(EVENT_RECEIVE_MESSAGE, message => {
   drawMessage(message)
 })
 
-chatMessage.addEventListener('keyup', event => { 
+socket.on(EVENT_SEND_MESSAGE_GLOBAL, message => {
+  drawGlobalMessage(message)
+})
+
+chatMessageInput.addEventListener('keyup', event => { 
   if (event.key === 'Enter' || event.keyCode === 13) {
     let message = {
       author: socket.id,
-      content: chatMessage.value
+      content: chatMessageInput.value
     }
-    socket.emit('sendMessage', message)
-    chatMessage.value = ''
+    socket.emit(EVENT_SEND_MESSAGE, message)
+    chatMessageInput.value = ''
     drawMessage(message)
   }
  })
--- server.js
+++ server.js
@@ -4,21 +4,31 @@ import http from 'http'
 import { Server } from 'socket.io'
 
 import manager from './public/manager.js'
+import {
+  PORT,
+  SUBDOMAIN,
+  PREFIX_SERVER,
+  EVENT_CONNECTION,
+  EVENT_DISCONNECT,
+  EVENT_RECONNECT,
+  EVENT_SEND_MESSAGE,
+  EVENT_SEND_MESSAGE_GLOBAL,
+  EVENT_PREVIOUS_MESSAGES,
+  EVENT_UPDATE_PLAYERS,
+  EVENT_RECEIVE_MESSAGE
+} from './public/constants.js'
 
-const PORT = 3333;
-const SUBDOMAIN = 'lorby';
-const PREFIX = '[LORBY SERVER]';
 
 const app = express()
-const server = http.createServer(app)
-const socket = new Server(server)
+const httpServer = http.createServer(app)
+const socketServer = new Server(httpServer)
 
 const gameManager = manager()
 
 /* const tunnel = localtunnel(PORT, { subdomain: SUBDOMAIN}, (err, tunnel) => {
-  console.log(`${PREFIX} Tunnel is running on ${tunnel.url}`)
+  console.log(`${PREFIX_SERVER} Tunnel is running on ${tunnel.url}`)
   if (err) {
-    console.log(`${PREFIX} Error: ${err}`)
+    console.log(`${PREFIX_SERVER} Error: ${err}`)
   }
 })
  */
@@ -26,30 +36,55 @@ app.use(express.static('public'))
 
 let messages = []
 
-socket.on('connection', (socket) => {
-  console.log(`${PREFIX} New connection: ${socket.id}`)
-
+socketServer.on(EVENT_CONNECTION, (socket) => {
   gameManager.players.push({
     id: socket.id,
     name:`player_${socket.id}}`,
     position:[Math.random(500), Math.random(500)]
   })
 
-  socket.emit('previousMessages', messages)
+  console.log(gameManager.players.length)
 
-  socket.on('sendMessage', (message) => {
+  socketServer.emit(EVENT_UPDATE_PLAYERS, gameManager.players)
+  socket.emit(EVENT_PREVIOUS_MESSAGES, messages)
+  
+  console.log(`${PREFIX_SERVER} New connection: ${socket.id}`)
+
+  socket.on(EVENT_SEND_MESSAGE, (message) => {
     console.log(message)
     messages.push(message)
-    socket.broadcast.emit('receivedMessage', message)
+    socket.broadcast.emit(EVENT_RECEIVE_MESSAGE, message)
   })
 
+  socket.on(EVENT_DISCONNECT, () => {
+    const oldPlayerIndex = gameManager.players.findIndex(player => player.id === socket.id)
+    
+    if (oldPlayerIndex !== -1) {
+      const oldPlayer = gameManager.players.splice(oldPlayerIndex, 1)[0]
+      
+      if (oldPlayer) {
+        socket.broadcast.emit(EVENT_SEND_MESSAGE_GLOBAL, {
+          author: PREFIX_SERVER,
+          content: `${oldPlayer.name} has left the game.`
+        })
+        
+        socket.broadcast.emit(EVENT_UPDATE_PLAYERS, gameManager.players)
+        
+        console.log(`${PREFIX_SERVER} Disconnected: ${socket.id}`)
+      }
+    }
+  })
+  
+  socket.on('reconnect', () => {
+    console.log(`${PREFIX_SERVER} Reconnected: ${socket.id}`)
+  })
 })
 
-server.listen(3333, () => {
-  console.log(`${PREFIX} Server is running on port ${PORT}`)
+httpServer.listen(PORT, () => {
+  console.log(`${PREFIX_SERVER} Server is running on port ${PORT}`)
 })
 
 /* tunnel.on('close', () => {
-  console.log(`${PREFIX} Tunnel is closed`)
+  console.log(`${PREFIX_SERVER} Tunnel is closed`)
 })
  */
