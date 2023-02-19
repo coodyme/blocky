c6b31ab9e300d787b06ed42125d4e802e4c307c1
augustobritodev <augustobritodev@icloud.com>
2023-02-19 15:59:22 -0300

    chore: add skins

--- public/client.js
+++ public/client.js
@@ -5,28 +5,65 @@ const primaryColor = getComputedStyle(document.documentElement)
   .getPropertyValue('--primary')
 const secondaryColor = getComputedStyle(document.documentElement)
   .getPropertyValue('--secondary')
-const foregroundColor = getComputedStyle(document.documentElement)
+  const backgroundColor = getComputedStyle(document.documentElement)
+  .getPropertyValue('--background')
+  const foregroundColor = getComputedStyle(document.documentElement)
   .getPropertyValue('--foreground')
 
 const context = canvas.getContext('2d')
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
+const TILE_SIZE = 64
+const PLAYER_SIZE = 32
 
 canvas.width = WIDTH
 canvas.height = HEIGHT 
 
 let lastTimestamp = 0
+let clientMap = []
+let clientPlayers = []
 
+clientSocket.on('setup', (map) => {
+  clientMap = map
+})
+
+clientSocket.on('players', (players) => {
+  clientPlayers = players
+})
 
 function update(delta) {
 
 }
 
 function draw() {
-  context.fillStyle = foregroundColor
   context.clearRect(0, 0, WIDTH, HEIGHT)
-  context.fillRect(50, 50, 30, 30)
+  
+    context.fillStyle = foregroundColor
+    for (let row = 0; row < clientMap.length; row++) {
+      for (let col = 0; col < clientMap[row].length; col++) {
+        const tile = clientMap[row][col]
+
+        if (tile === 1) {
+          context.fillRect(
+            col * TILE_SIZE,
+            row * TILE_SIZE,
+            TILE_SIZE,
+            TILE_SIZE
+          )
+        }
+      }
+    }
+
+    for (let player of clientPlayers) {
+      context.fillStyle = ['red', 'blue', 'green', 'yellow'][Math.floor(Math.random() * 4)]
+      context.fillRect(
+        player.position.x,
+        player.position.y,
+        PLAYER_SIZE,
+        PLAYER_SIZE
+      )
+    }
 }
 
 function loop(timestamp) {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import { setInterval } from 'timers'
 
 const PORT = process.env.PORT || 3333
+const GRAVITY = 0.00981
+const TICK_RATE = 50
 
 const app = express()
 const httpServer = http.createServer(app)
@@ -10,13 +13,66 @@ const socketServer = new Server(httpServer)
 
 app.use(express.static('public'))
 
+const MAP = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+const PLAYERS = []
+const SKINS = ['lorbiroto', 'lorbirinha', 'milos', 'lorbiman', 'lorbipresso']
+
 socketServer.on('connect', (socket) => {
-  console.log(`Player connect with id: ${ socket.id}`)
+  console.log(`Player connected with id: ${ socket.id}`)
+
+  socket.emit('setup', MAP)
+
+  PLAYERS.push({
+    id: socket.id,
+    name: `player_${SKINS[Math.floor(Math.random() * SKINS.length)]}`,
+    skin: SKINS[Math.floor(Math.random() * SKINS.length)],
+    position: { x: Math.floor(Math.random() * 100), y: 0},
+    velocity: { x: 0, y: 0},
+    size: { x: 64, y: 64 },
+  })
 
   socket.on('disconnect', (reason) => {
-    console.log(`Player disconnect with reason: ${reason}`)
+    const index = PLAYERS.findIndex(player => player.id === socket.id)
+    
+    if (index !== -1) {
+      const player = PLAYERS.splice(index, 1)[0]
+      
+      if (player) {
+        console.log(`Player: ${player.name} disconnected with reason: ${reason}`)
+      }
+    }
+    
   })
 })
+
+const loop = (deltaTime) => {
+  for (let player of PLAYERS) {
+    player.velocity.y += GRAVITY * deltaTime
+    player.position.y = player.velocity.y
+  }  
+
+  socketServer.emit('players', PLAYERS)
+}
+
+let lastTime = performance.now()
+setInterval(() => {
+  let now = performance.now()
+  let deltaTime = now - lastTime
+  loop(deltaTime) 
+  lastTime = performance.now()
+}, 1000 / TICK_RATE)
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
